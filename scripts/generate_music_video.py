8c43ae5cfbd0bbfee81c6acf8836ffea3567b9fc
vitas <82312336@kt.com>
2026-08-16 17:12:57 +0900

    feat: change piano generation to use random chords

--- scripts/generate_music_video.py
+++ scripts/generate_music_video.py
@@ -32,13 +32,23 @@ def generate_piano_music(duration_sec, output_path):
     buffer_l = np.zeros(total_samples, dtype=np.float32)
     buffer_r = np.zeros(total_samples, dtype=np.float32)
     
-    # C major pentatonic (C3 ~ C6)
-    melody_midi = [48, 50, 52, 55, 57, 60, 62, 64, 67, 69, 72, 74, 76, 79, 81, 84]
-    melody_freqs = [440.0 * (2.0 ** ((m - 69.0) / 12.0)) for m in melody_midi]
+    # C, F, G, Am, Dm, Em (기본 3화음)
+    chords_midi = [
+        ([48, 52, 55], 36), # C
+        ([53, 57, 60], 41), # F
+        ([55, 59, 62], 43), # G
+        ([57, 60, 64], 45), # Am
+        ([50, 53, 57], 38), # Dm
+        ([52, 55, 59], 40), # Em
+    ]
     
-    # 베이스 용 저음역대 (C2 ~ C4)
-    bass_midi = [36, 38, 40, 43, 45, 48, 50, 52]
-    bass_freqs = [440.0 * (2.0 ** ((m - 69.0) / 12.0)) for m in bass_midi]
+    def midi_to_freq(m):
+        return 440.0 * (2.0 ** ((m - 69.0) / 12.0))
+
+    chords_data = [
+        ([midi_to_freq(m) for m in chord], midi_to_freq(bass))
+        for chord, bass in chords_midi
+    ]
     
     # 템포 설정 (BPM 120)
     bpm = 120
@@ -46,33 +56,41 @@ def generate_piano_music(duration_sec, output_path):
     beat_samples = int(fs * beat_sec)
     
     current_sample = 0
+    current_measure = -1
+    current_chord_freqs, current_bass_freq = [], 0
+    
     while current_sample < total_samples:
-        current_beat = current_sample // beat_samples
+        measure = current_sample // (beat_samples * 4)
         
-        # 1. 베이스 연주 (4박자마다 80% 확률로)
-        if current_beat % 4 == 0 and random.random() < 0.8:
-            freq = random.choice(bass_freqs)
-            note_duration = random.uniform(3.0, 5.0)
-            note_wave = generate_piano_note(freq, note_duration, fs)
+        # 1. 4박자(1마디)마다 새로운 코드 선택 및 베이스 연주
+        if measure > current_measure:
+            current_measure = measure
+            current_chord_freqs, current_bass_freq = random.choice(chords_data)
             
-            pan = random.uniform(0.4, 0.6)
-            volume = random.uniform(0.3, 0.5)
-            
-            end_sample = current_sample + len(note_wave)
-            if end_sample > total_samples:
-                note_wave = note_wave[:total_samples - current_sample]
-                end_sample = total_samples
+            if random.random() < 0.8:
+                note_duration = random.uniform(3.0, 5.0)
+                note_wave = generate_piano_note(current_bass_freq, note_duration, fs)
                 
-            buffer_l[current_sample:end_sample] += note_wave * (1.0 - pan) * volume
-            buffer_r[current_sample:end_sample] += note_wave * pan * volume
-            
-        # 2. 멜로디 연주 (65% 확률)
+                pan = random.uniform(0.4, 0.6)
+                volume = random.uniform(0.3, 0.5)
+                
+                end_sample = current_sample + len(note_wave)
+                if end_sample > total_samples:
+                    note_wave = note_wave[:total_samples - current_sample]
+                    end_sample = total_samples
+                    
+                buffer_l[current_sample:end_sample] += note_wave * (1.0 - pan) * volume
+                buffer_r[current_sample:end_sample] += note_wave * pan * volume
+                
+        # 2. 코드 기반 멜로디 연주 (65% 확률)
         if random.random() < 0.65:
-            play_double = random.random() < 0.25  # 25% 확률로 2화음
-            
-            notes_to_play = [random.choice(melody_freqs)]
-            if play_double:
-                notes_to_play.append(random.choice(melody_freqs))
+            # 코드를 통째로 칠지, 1~2음만 칠지
+            play_full_chord = random.random() < 0.4
+            if play_full_chord:
+                notes_to_play = current_chord_freqs
+            else:
+                num_notes = random.randint(1, 2)
+                notes_to_play = random.sample(current_chord_freqs, num_notes)
                 
             for freq in notes_to_play:
                 note_duration = random.choice([1.0, 1.5, 2.0, 3.0])
@@ -89,7 +107,7 @@ def generate_piano_music(duration_sec, output_path):
                 buffer_l[current_sample:end_sample] += note_wave * (1.0 - pan) * volume
                 buffer_r[current_sample:end_sample] += note_wave * pan * volume
         
-        # 박자 간격 이동
+        # 박자 간격 이동 (8분음표 또는 4분음표 길이로 이동)
         if random.random() < 0.4:
             current_sample += beat_samples // 2
         else:
